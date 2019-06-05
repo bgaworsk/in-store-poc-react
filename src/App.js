235205a1a8cdf481e4d7a8e680fadd24ea58135d
Björn Gaworski-Dammann <bg@tty7.de>
2019-06-05 21:23:31 +0200

    added CoreMedia CI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import styled from 'styled-components';
+import ciColours from './lib/CoreMediaColours';
+import coreLight from './fonts/core-light-woff2-data.woff2';
+import simplonNormRegular from './fonts/simplonnorm-regular-webxl-woff2-data.woff2';
 
 const AppWrapper = styled.div`
+  @font-face {
+  font-family: "Core Light";
+  font-weight: 500;
+  font-style: normal;
+  src: url(${coreLight}) format("woff2");
+}
+
+@font-face {
+  font-family: "Simplon Norm Regular";
+  font-weight: 500;
+  font-style: normal;
+  src: url(${simplonNormRegular}) format("woff2");
+}
+
+  height: 100%;
   text-align: center;
   
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
+  font-family: "Simplon Norm Regular", "Lucida Sans", "Lucida Sans Unicode", "Lucida Grande", Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   
@@ -18,19 +34,19 @@ const AppWrapper = styled.div`
 `;
 
 const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
+  background-color: ${ciColours.purple};
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
+  font-size: calc(10px + 1vmin);
   color: white;
 `;
 
 const AppLogo = styled.img`
   animation: App-logo-spin infinite 20s linear;
-  height: 40vmin;
+  margin-top: 5%;
   pointer-events: none;
   
   @keyframes App-logo-spin {
@@ -44,7 +60,9 @@ const AppLogo = styled.img`
 `;
 
 const AppLink = styled.a`
-  color: #61dafb;
+font-family: "Core Light", "Simplon Norm Regular", "Lucida Sans", "Lucida Sans Unicode", "Lucida Grande", Arial, Helvetica, sans-serif;
+  color: ${ciColours.turquoise};
+  margin-bottom: 5%;
 `;
 
 function App() {
@@ -53,15 +71,15 @@ function App() {
       <AppHeader>
         <AppLogo src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proof-of-concept of <code>standalone</code> CoreMedia widgets.
         </p>
         <AppLink
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coremedia.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Checkout CoreMedia
         </AppLink>
       </AppHeader>
     </AppWrapper>
---
+++ src/lib/CoreMediaColours.js
@@ -0,0 +1,10 @@
+export default {
+
+  turquoise: '#6FC3B8',
+  purple: '#5f2e75',
+  red: '#DD342B',
+  yellow: '#ecdd4c',
+  blue: '#006cae',
+  green: '#2FAC66'
+
+}
