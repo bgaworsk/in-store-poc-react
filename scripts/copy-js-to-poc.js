0df86739dd475ac4dc7f3d534179c37c9b7a10a1
Björn Gaworski-Dammann <bg@tty7.de>
2019-06-05 21:26:42 +0200

    Renamed PoC folder to docs so that its content get served by github

--- scripts/copy-js-to-poc.js
+++ scripts/copy-js-to-poc.js
@@ -9,7 +9,7 @@ const pattern = /^main\.\w+\.js$/;
 // Copy file main.*.js to poc folder with name main.js
 fs.readdirSync(path.join(appBuild, 'static', 'js')).map(file => {
   if (pattern.test(file)){
-    fs.copyFileSync(path.join(appBuild, 'static', 'js', file), path.join(appPath, 'poc-embed-standalone', 'main.js'));
+    fs.copyFileSync(path.join(appBuild, 'static', 'js', file), path.join(appPath, 'docs', 'main.js'));
   }
 });
 
