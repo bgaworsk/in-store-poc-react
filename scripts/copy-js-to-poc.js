add37e05c7b7f34101eb21cf8d6a7b8b73050573
Björn Gaworski-Dammann <bg@tty7.de>
2019-06-05 20:48:59 +0200

    Add script to copy build JS to PoC html folder

---
+++ scripts/copy-js-to-poc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { appPath, appBuild } = require('../config/paths');
+
+const pattern = /^main\.\w+\.js$/;
+
+// Copy file main.*.js to poc folder with name main.js
+fs.readdirSync(path.join(appBuild, 'static', 'js')).map(file => {
+  if (pattern.test(file)){
+    fs.copyFileSync(path.join(appBuild, 'static', 'js', file), path.join(appPath, 'poc-embed-standalone', 'main.js'));
+  }
+});
+
+
