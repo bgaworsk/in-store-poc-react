c02df636bf628d3e0a7319accd5d4eea009d29e6
Björn Gaworski <bg@tty7.de>
2019-06-11 16:04:48 +0200

    Copied build folder into github docs

---
+++ docs/poc/precache-manifest.e64bf3fcd75c453a179736822b3cfefe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d001ce63bb9212d1ae2a188ba89a55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6a5df4df0d773c15d02",
+    "url": "/static/js/main.3d40588f.js"
+  },
+  {
+    "revision": "a31b756fdb73dd19d6d78b7171bdfa0d",
+    "url": "/static/media/core-light-woff2-data.a31b756f.woff2"
+  },
+  {
+    "revision": "a1bed9760312fdddad5243a3c194866b",
+    "url": "/static/media/simplonnorm-medium-webxl-woff2-data.a1bed976.woff2"
+  },
+  {
+    "revision": "16ac8ac835eb1ffe78f6bc5f366461c6",
+    "url": "/static/media/simplonnorm-regular-webxl-woff2-data.16ac8ac8.woff2"
+  }
+]);
